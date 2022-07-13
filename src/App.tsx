8971ab4560b39a9ddee4c76db41209d4eb15e4d8
kujawapatryk <homik363@wp.pl>
2022-07-14 00:07:11 +0200

    Init commit.

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import logo from './logo.svg';
+import {Test} from 'types';
+import './App.css';
+
+function App() {
+  const foobar: Test ={
+  x:20,
+  };
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
